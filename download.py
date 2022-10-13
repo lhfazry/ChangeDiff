cfab496e5659344fcf5373e76237b371c66b2a9b
Fazry <lhuqita.fazry@gmail.com>
2022-10-13 09:33:16 +0700

    first commit

--- download.py
+++ download.py
@@ -9,3 +9,6 @@ Path("models").mkdir(exist_ok=True)
 
 if not os.path.exists('models/LEVIR_ema_0.9999_650000.pt'):
     gdown.download(id="1bcnQE5XiU8CMIzzoykQbgW9oFBX3y1xC", output="models/LEVIR_ema_0.9999_650000.pt", quiet=False)
+
+if not os.path.exists('models/DSIFN_ema_0.9999_840000.pt'):
+    gdown.download(id="1lL6HjQRvR7Ld0hAU3s74CTACSLV7SJnN", output="models/DSIFN_ema_0.9999_840000.pt", quiet=False)
